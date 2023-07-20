800d205231de0e65ef52dc26286f6905ff79783c
linuxmobile <bdiez19@gmail.com>
2023-07-20 03:41:46 -0300

    Fix: fixing build

---
+++ pkgs/default.nix
@@ -0,0 +1,16 @@
+{
+  systems = ["x86_64-linux"];
+
+  perSystem = {pkgs, ...}: {
+    packages = {
+      # instant repl with automatic flake loading
+      repl = pkgs.callPackage ./repl {};
+
+      catppuccin-plymouth = pkgs.callPackage ./catppuccin-plymouth {};
+
+      # gdb-frontend = pkgs.callPackage ./gdb-frontend {};
+
+      # waveform = pkgs.callPackage ./waveform {};
+    };
+  };
+}
