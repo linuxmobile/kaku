4bb4350d888b6fdf64f91847bbefb17aab09c746
linuxmobile <bdiez19@gmail.com>
2023-07-22 13:53:50 -0300

    Fix: fixing nvchad package

--- home/editors/nvchad/default.nix
+++ home/editors/nvchad/default.nix
@@ -1,37 +1,14 @@
-{ lib, config, pkgs, ... }:
-
-with lib;
-
-let
-  cfg = config.modules.nvim;
-in
-
 {
-  options.modules.nvim = { enable = mkEnableOption "nvim"; };
-  config = mkIf cfg.enable {
-    xdg.configFile."nvim" = {
-      source = "${pkgs.nvchad}";
-      recursive = true;
-    };
-    home.packages = with pkgs; [
-      # lua stuff
-      lua-language-server
-      stylua
+  pkgs,
+  inputs',
+  self',
+  ...
+}: {
+  programs.neovim = {
+    enable = true;
+  };
 
-      # web dev
-      nodePackages.typescript-language-server
-      nodePackages.tailwindcss
-      nodePackages.prettier
-      deno
-      nil
-      rnix.lsp
-      marksman
-    ];
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-    };
+  home.file = {
+    ".config/nvim/".source = "${self'.packages.nvchad}";
   };
 }
