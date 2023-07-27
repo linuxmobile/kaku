48c0c26b2018b426d58fd25f1d671d7f57f71f75
linuxmobile <bdiez19@gmail.com>
2023-07-26 23:33:05 -0300

    Fix: fixing bat theme

--- home/shell/cli.nix
+++ home/shell/cli.nix
@@ -32,9 +32,9 @@
       };
       themes = {
         Catppuccin-mocha = builtins.readFile (pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
-          hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
-        });
+        url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
+        hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
+      });
       };
     };
 
