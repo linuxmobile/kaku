0b241c93938309867f63e0783bd136baa048b6db
linuxmobile <bdiez19@gmail.com>
2023-07-27 13:29:52 -0300

    Update: updating spicetify opts

--- home/software/spicetify.nix
+++ home/software/spicetify.nix
@@ -1,18 +1,31 @@
-{inputs', ...}: {
+{inputs',self', ...}: {
   # themable spotify
   programs.spicetify = let
     spicePkgs = inputs'.spicetify-nix.packages.default;
   in {
     enable = true;
-
+    injectCss = true;
+    replaceColors = true;
+    overwriteAssets = true;
+    sidebarConfig = true;
+    enabledCustomApps = with spicePkgs.apps; [
+      lyrics-plus
+      new-releases
+    ];
     theme = spicePkgs.themes.catppuccin-mocha;
-
     colorScheme = "mauve";
-
     enabledExtensions = with spicePkgs.extensions; [
       hidePodcasts
       shuffle
       adblock
+      playlistIcons
+      lastfm
+      genre
+      historyShortcut
+      bookmark
+      fullAlbumDate
+      groupSession
+      popupLyrics
     ];
   };
 }
