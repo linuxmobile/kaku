47b513fb16902fd907364d21cace417cb601df1b
Braian A. Diez <bdiez19@gmail.com>
2024-05-10 20:04:56 -0300

    feat: Update theme file path and handle missing file error

--- home/services/system/auto-theme.nix
+++ home/services/system/auto-theme.nix
@@ -4,21 +4,23 @@
   ...
 }: let
   script = pkgs.writeShellScript "theme-changer.sh" ''
-    HELIX_THEME="${config.xdg.configHome}/helix/current.toml"
+    HELIX_THEME="${config.xdg.configHome}/helix/themes/current.toml"
 
-    while true; do
-      THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
-      case "$THEME" in
-        "'prefer-dark'")
-          sed -i 's/^\s*inherits = .*/inherits = "mocha"/' $HELIX_THEME
-          ;;
-        "'prefer-light'")
-          sed -i 's/^\s*inherits = .*/inherits = "latte"/' $HELIX_THEME
-          ;;
+    if [ ! -f "$HELIX_THEME" ]; then
+      echo "El archivo de tema de Helix no existe: $HELIX_THEME"
+      exit 1
+    fi
+
+    gsettings monitor org.gnome.desktop.interface color-scheme | while read -r line; do
+      COLOR_SCHEME=$(echo $line | awk '{print $2}')
+      case "$COLOR_SCHEME" in
+      "'prefer-dark'")
+        sed -i 's/^inherits = .*/inherits = "mocha"/' $HELIX_THEME
+        ;;
+      "'default'")
+        sed -i 's/^inherits = .*/inherits = "latte"/' $HELIX_THEME
+        ;;
       esac
-
-      # wait for the theme to change
-      gsettings monitor org.gnome.desktop.interface color-scheme > /dev/null
     done
   '';
 in {
