b813fd9be27dc32720c85f1a87cdf5f3b7465404
Braian A. Diez <bdiez19@gmail.com>
2024-05-30 17:41:58 -0300

    feat: Add swapfile with random encryption

--- hosts/aesthetic/hardware-configuration.nix
+++ hosts/aesthetic/hardware-configuration.nix
@@ -26,7 +26,11 @@
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [ {
+     device = "/var/lib/swapfile";
+     size = 16*1024;
+     randomEncryption.enable = true;
+   } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
