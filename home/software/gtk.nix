191366557d0a6207c8c8f40c6e9eebf14b27cf42
Braian A. Diez <bdiez19@gmail.com>
2024-07-05 18:27:14 -0300

    feat: Update GTK configuration to disable custom CSS file

--- home/software/gtk.nix
+++ home/software/gtk.nix
@@ -27,36 +27,17 @@
       package = pkgs.colloid-icon-theme;
     };
 
-    theme = {
-      name = "WhiteSur-Dark";
-      package = pkgs.whitesur-gtk-theme.override {
-        nautilusStyle = "glassy";
-      };
-    };
     gtk3 = {
       bookmarks = [
+        "file://${config.home.homeDirectory}/Dev"
         "file://${config.home.homeDirectory}/Documents"
         "file://${config.home.homeDirectory}/Downloads"
         "file://${config.home.homeDirectory}/Music"
         "file://${config.home.homeDirectory}/Pictures"
         "file://${config.home.homeDirectory}/Videos"
       ];
-      extraConfig = {
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintfull";
-        gtk-xft-rgba = "rgb";
-        gtk-application-prefer-dark-theme = 1;
-      };
     };
 
-    gtk2.extraConfig = ''
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle="hintslight"
-      gtk-xft-rgba="rgb"
-    '';
-
-    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+    xdg.configFile."gtk-4.0/gtk.css".enable = lib.mkForce false;
   };
 }
