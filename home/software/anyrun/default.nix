d64fe0bf4b9186f57f0cb055a8519bdac3c412b2
Braian A. Diez <bdiez19@gmail.com>
2024-07-05 18:27:32 -0300

    feat: Update Anyrun UI styles and configuration

--- home/software/anyrun/default.nix
+++ home/software/anyrun/default.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   inputs,
-  config,
   ...
 }: {
   imports = [
@@ -15,23 +14,32 @@
       plugins = with inputs.anyrun.packages.${pkgs.system}; [
         applications
         shell
+        symbols
         translate
       ];
 
-      width.fraction = 0.3;
-      y.absolute = 15;
+      width.fraction = 0.25;
+      y.fraction = 0.3;
       hidePluginInfo = true;
       closeOnClick = true;
     };
 
     extraCss = builtins.readFile (./. + "/style-dark.css");
 
-    extraConfigFiles."applications.ron".text = ''
-      Config(
-        desktop_actions: false,
-        max_entries: 5,
-        terminal: Some("wezterm"),
-      )
-    '';
+    extraConfigFiles = {
+      "applications.ron".text = ''
+        Config(
+          desktop_actions: false,
+          max_entries: 5,
+          terminal: Some("foot"),
+        )
+      '';
+
+      "shell.ron".text = ''
+        Config(
+          prefix: ">"
+        )
+      '';
+    };
   };
 }
