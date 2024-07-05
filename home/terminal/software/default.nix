38c35c598957f0abd9833dc7e1a304af355449d5
Braian A. Diez <bdiez19@gmail.com>
2024-07-05 18:28:13 -0300

    chore: remove unused dependencies and programs

--- home/terminal/software/default.nix
+++ home/terminal/software/default.nix
@@ -2,19 +2,14 @@
   imports = [
     ./bat.nix
     ./bottom.nix
-    ./cava.nix
     ./cli.nix
     ./gpg.nix
     ./git.nix
     ./gitui.nix
-    # ./inlyne.nix
     ./neofetch.nix
     ./nix.nix
-    # ./screenshot-area.nix
-    # ./screenshot-full.nix
     ./skim.nix
     ./transient-services.nix
-    # ./twitch-hls-client.nix
     ./yazi
     ./xdg.nix
     ./zoxide.nix
