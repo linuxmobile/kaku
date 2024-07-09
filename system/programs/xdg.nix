8074903da4ce4fdb5c32d6f42ccd7f8ccc068fca
Braian A. Diez <bdiez19@gmail.com>
2024-07-09 13:24:51 -0300

    chore: Update xdg.nix to remove unused xdg.portal configuration

--- system/programs/xdg.nix
+++ system/programs/xdg.nix
@@ -2,11 +2,6 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    config = {
-      common.default = ["gtk"];
-      gnome.default = ["gtk" "gnome"];
-    };
-
     extraPortals = [
       pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
