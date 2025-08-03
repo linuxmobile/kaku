256387cb2b42e81f7f2cc5663b357944403bf293
Braian A. Diez <bdiez19@gmail.com>
2025-08-03 19:47:27 -0300

    refactor(system/services): location
    trying to improve this shit

--- system/services/location.nix
+++ system/services/location.nix
@@ -1,11 +1,12 @@
 {
   # enable location service
-  location = {
-    provider = "geoclue2";
-    latitude = -38.38;
-    longitude = -60.26;
-  };
+  location.provider = "geoclue2";
 
   # provide location
-  services.geoclue2.enable = true;
+  services.geoclue2 = {
+    enable = true;
+    geoProviderUrl = "https://beacondb.net/v1/geolocate";
+    submissionUrl = "https://beacondb.net/v2/geosubmit";
+    submissionNick = "geoclue";
+  };
 }
